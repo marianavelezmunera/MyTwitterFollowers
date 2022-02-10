4755fba0fdb632fc231823196daed6b21e8782d3
Mariana <marianavelez.35@gmail.com>
2022-02-10 17:12:08 -0500

    commit

--- Gender.R
+++ Gender.R
@@ -1,10 +1,21 @@
 #Género de mis seguidores
-
+library(ggplot2)
+library(MetBrewer)
 genero<-c("Hombre","Mujer","No binario")
 f_genero<-c(242,494,16)
 datos_genero<-data.frame(genero,f_genero)
 colnames(datos_genero)<-c("Genero","Frecuencia")
-library(plotrix)
-tortalabels<-c("Hombre","Mujer","NB")
-torta<-pie3D(datos_genero$Frecuencia,edges=100, explode=0.1, radius=2, height=0.3,theta=0.5, start=0, labelcol="black", labelcex=1, main = "Género de mis seguidores", cex.main=1.55,labels = tortalabels,col = met.brewer("Cassatt1",3,type = "discrete"),labelpos=c(80,20,30))
+text<-"N=752"
 
+ggplot(datos_genero,aes(x="",y=Frecuencia,fill=Genero))+
+  geom_bar(stat="identity",width = 1)+
+  coord_polar(theta = "y",start = 0)+
+  theme_void()+
+  ggtitle("Género de mis seguidores")+
+  theme(plot.title=element_text( hjust=1, vjust=0.5, face='bold'))+
+  scale_fill_manual(values=met.brewer("Klimt", 3))+
+  geom_text(data = NULL, x = 1.9, y = 105, label = "N=752")+
+  geom_text(data = NULL, x = 1, y = 10, label = "16")+
+  geom_text(data = NULL, x = 1, y = 200, label = "494")+
+  geom_text(data = NULL, x = 0, y = 200, label = "242")
+  
